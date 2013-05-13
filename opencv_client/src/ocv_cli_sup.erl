8fa8a4b6119045ac72d72abf15551fc88673a75d
Edward Tate <edward.tate@erlang-solutions.com>
2013-05-13 12:47:57 +0100

    Set supervisor to correct gen_server child.

--- opencv_client/src/ocv_cli_sup.erl
+++ opencv_client/src/ocv_cli_sup.erl
@@ -24,5 +24,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+  Child = ?CHILD(ocv_cli_server, worker),
+  {ok, { {one_for_one, 5, 10}, [Child]} }.
 
