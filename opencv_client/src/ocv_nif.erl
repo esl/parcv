0ff171f38e7e7f1dda470cc05896c588380de91d
Edward Tate <edward.tate@erlang-solutions.com>
2013-06-10 13:57:28 +0100

    Added data send loop to ocv_nif.

--- opencv_client/src/ocv_nif.erl
+++ opencv_client/src/ocv_nif.erl
@@ -3,6 +3,7 @@
 -export([any_device/0]).
 -export([new_frame/1, query_frame/2]).
 -export([frame_to_tuple/1]).
+-export([loop_send_frames/0]).
 
 -on_load(init/0).
 
@@ -18,6 +19,17 @@ init() ->
 		Path
 	    end,
   erlang:load_nif("." ++ filename:join(PrivDir, "ocv_drv"), 0).
+
+send_frames(D, F) ->
+  ok = query_frame(D, F),
+  {W, H, NChan, ImageSize, Data} = frame_to_tuple(F),
+  ocv_cli_server:send_data(Data),
+  send_frames(D, F).
+
+loop_send_frames() ->
+  {ok, D} = any_device(),
+  {ok, F} = new_frame(D),
+  send_frames(D, F).
 
 any_device() ->
   ?NIF_STUB.
